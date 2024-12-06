acffae8f96ea969863f2c6a6b76c941ee753db58
Eduardo Kurek <eduardokurek2003@gmail.com>
2024-12-05 23:11:09 -0300

    Adicionando validações do arquivo de entrada

--- parser/main.c
+++ parser/main.c
@@ -1,24 +1,95 @@
 #include "test.tab.h"
 #include "tree.h"
+#include "errors.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool check_key = false;
 
 extern Node* syntax_tree;
 extern FILE* yyin;
 
+void yyfatal_error(const Error* err);
+bool stringHasPoint(const char* str);
+bool hasTppExtension(const char* str);
+bool stringIsK(const char* str);
+int handleArguments(int argc, char* argv[]);
+void redirectYyin(const char* fileName);
+
 int main(int argc, char* argv[]) {
-    if(argc < 2) {
-        printf("Usage: %s <input_file>\n", argv[0]);
-        return 1;
-    }
+    int fileIdx = handleArguments(argc, argv);
+    redirectYyin(argv[fileIdx]);
 
-    yyin = fopen(argv[1], "r");
+    yyin = fopen(argv[fileIdx], "r");
 
-	do{
-		yyparse();
-	} while(!feof(yyin));
+    do {
+        yyparse();
+    } while(!feof(yyin));
 
     node_to_dot(syntax_tree, "tree.dot");
 
     return 0;
+}
+
+void yyfatal_error(const Error* err){
+    if(check_key)
+        printf("%s\n", err->cod);
+    else 
+        printf("%s\n", err->msg);
+    exit(1);
+}
+
+bool stringHasPoint(const char *str){
+    if(strstr(str, ".") == NULL) return false;
+    return true;
+}
+
+bool hasTppExtension(const char* str) {
+    char buffer[256];  
+    strncpy(buffer, str, sizeof(buffer));
+    buffer[sizeof(buffer) - 1] = '\0';
+
+    char *token = strtok(buffer, ".");
+    char *last_token = NULL;
+
+    while (token != NULL) {
+        last_token = token;
+        token = strtok(NULL, ".");
+    }
+
+    return (last_token != NULL && strcmp(last_token, "tpp") == 0);
+}
+
+bool stringIsK(const char* str) {
+    return !strcmp(str, "-k");
+}
+
+int handleArguments(int argc, char* argv[]) {
+    int fileIdx = -1;
+
+    for(int i = 1; i < argc; i++) {
+        char* arg = argv[i];
+
+        if(stringIsK(arg)){
+            check_key = true;
+            continue;
+        }
+
+        if(stringHasPoint(arg)){
+            fileIdx = i;
+        }
+    }
+
+    if(fileIdx == -1) yyfatal_error(&ERR_SYN_USE);
+    if(!hasTppExtension(argv[fileIdx])) yyfatal_error(&ERR_SYN_NOT_TPP);
+    
+    return fileIdx;
+}
+
+void redirectYyin(const char* fileName){
+    FILE* file = fopen(fileName, "r");
+    if(file == NULL) yyfatal_error(&ERR_SYN_FILE_NOT_EXISTS);
+    yyin = file;
 }
